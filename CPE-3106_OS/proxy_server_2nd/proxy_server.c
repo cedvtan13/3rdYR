150cda0ec20d346a9ba7d03df6518d08ef69c1ed
cedvtan13 <cedv.tan@gmail.com>
2025-09-21 21:39:28 +0800

    hello

--- CPE-3106_OS/proxy_server_2nd/proxy_server.c
+++ CPE-3106_OS/proxy_server_2nd/proxy_server.c
@@ -15,7 +15,7 @@
 #define BUFFER_SIZE 4096
 #define SERVER_PORT 4444
 
-// extract host from HTTP request
+//xtract host from HTTP request
 void getHostfromReq(char *request, char *host){
     char *start = strstr(request, "Host: ");
     if (start) {
@@ -29,28 +29,28 @@ void getHostfromReq(char *request, char *host){
     }
 }
 
-// handle one client connection
+//handle one client connection
 void handle_client(int client_fd) {
     char buffer[BUFFER_SIZE];
     char host[256] = "";
     
-    // read HTTP request from client
+    //read HTTP request from client
     int bytes = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
     buffer[bytes] = '\0';
     
     printf("Got request from client\n");
     
-    // get the host from the request
+    //get the host from the request
     getHostfromReq(buffer, host);
     printf("Connecting to host: %s\n", host);
     
-    // create socket to connect to the real server
+    //create socket to connect to the real server
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     
-    // look up the server's IP address
+    //look up the server's IP address
     struct hostent *server = gethostbyname(host);
     
-    // connect to the real server (port 80)
+    //connect to real server port 80 ra ni im not sure why
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(80);
@@ -58,15 +58,15 @@ void handle_client(int client_fd) {
     connect(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
     printf("Connected to server!\n");
     
-    // send the original request to the real server
+    //send original request to real server
     send(server_fd, buffer, bytes, 0);
     
-    // forward response from server back to client
+    //forward response from server back to client
     while ((bytes = recv(server_fd, buffer, BUFFER_SIZE, 0)) > 0) {
         send(client_fd, buffer, bytes, 0);
     }
     
-    // close connections
+    //close
     close(server_fd);
     close(client_fd);
     printf("Done with this client\n");
@@ -97,7 +97,7 @@ int main(int argc, char *argv[]) {
     
     printf("Proxy listening on port %d\n", port);
     
-    // main loop: accept clients and fork for each one
+    //main loop: accept clients and fork for each one
     while (1) {
         struct sockaddr_in client_addr;
         socklen_t client_len = sizeof(client_addr);
@@ -109,7 +109,7 @@ int main(int argc, char *argv[]) {
         pid_t pid = fork();
         
         if (pid == 0) {
-            // this is the child process
+            //this is the child process
             close(listen_fd);
             handle_client(client_fd);
             exit(0);
